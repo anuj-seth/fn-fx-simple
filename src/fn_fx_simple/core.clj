91c2ddb204699d3121298146b8629ae0f70e95b9
anuj-seth <anujseth76@gmail.com>
2019-03-10 12:24:28 -0700

    fixed readme file, cleaned up the code

--- src/fn_fx_simple/core.clj
+++ src/fn_fx_simple/core.clj
@@ -7,19 +7,32 @@
   (:gen-class :extends
               javafx.application.Application))
 
-(defui TheButton
-  (render
-   [this args]
-   (ui/stack-pane :children [(ui/button :text (:text args)
-                                        :disable (:button-disabled? args)
-                                        :on-action {:event :button-press})])))
-
 (defn force-exit
   []
   (reify javafx.event.EventHandler
     (handle [this event]
       (shutdown-agents)
       (javafx.application.Platform/exit))))
+
+(defn button-press
+  [data-state]
+  (send data-state
+        assoc
+        :text "Said Hello"
+        :button-disabled? true)
+  (send data-state
+        (fn [the-data]
+          (Thread/sleep 5000)
+          (assoc the-data
+                 :text "Say 'Hello World'"
+                 :button-disabled? false))))
+
+(defui TheButton
+  (render
+   [this args]
+   (ui/stack-pane :children [(ui/button :text (:text args)
+                                        :disable (:button-disabled? args)
+                                        :on-action {:event :button-press})])))
 
 (defui TheStage
   (render
@@ -40,34 +53,24 @@
   [& args]
   (let [data-state (agent {:text "Say 'Hello World'"
                            :button-disabled? false })
-        handler-fn  (fn [{:keys [event]}]
-                      (condp = event
-                        :button-press (do
-                                        (send data-state
-                                              assoc
-                                              :text "Said Hello"
-                                              :button-disabled? true)
-                                        (send-off data-state
-                                                  (fn [the-data]
-                                                    (Thread/sleep 5000)
-                                                    (assoc the-data
-                                                           :text "Say 'Hello World'"
-                                                           :button-disabled? false))))))
+        handler-fn (fn [{:keys [event]}]
+                     (condp = event
+                       :button-press (button-press data-state)))
         ui-state (agent (dom/app
                          (the-stage @data-state)
-                         handler-fn))]
+                         handler-fn))
+        update-ui-state (fn [_ _ _ _]
+                          (send ui-state (fn [old-ui]
+                                           (dom/update-app old-ui
+                                                           (the-stage @data-state)))))]
     (add-watch data-state
                :ui
-               (fn [_ _ _ _]
-                 (send ui-state (fn [old-ui]
-                                  (dom/update-app old-ui
-                                                  (the-stage @data-state))))))))
+               update-ui-state)))
 
 (defn start-javafx
   [& args]
   (javafx.application.Application/launch fn_fx_simple.core
                                          (into-array String args)))
-
 
 (comment
   (require 'fn-fx.util.reflect-utils)
